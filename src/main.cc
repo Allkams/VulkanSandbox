7b4d5ef57ec0d635c0eb3e8ac542cf5284685c7a
Allkams <Allkams02@gmail.com>
2025-03-26 13:15:58 +0100

    Vulkan instancing

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -46,6 +47,51 @@ private:
 		appInfo.pEngineName = "No Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtentionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtentionCount);
+
+		//createInfo.enabledExtensionCount = glfwExtentionCount;
+		//createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		std::vector<const char*> requiredExtensions;
+
+		for (uint32_t i = 0; i < glfwExtentionCount; i++)
+		{
+			requiredExtensions.emplace_back(glfwExtensions[i]);
+		}
+
+		requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+		createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+		createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+		createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+		//VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "Available extensions:\n";
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << std::endl;
+		}
 	}
 
 	void mainLoop()
@@ -59,6 +105,8 @@ private:
 
 	void cleanup()
 	{
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
